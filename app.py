c2b6807a3823ae31258b3e693d58b5bb065cc7e0
dianeapioodongo <69053548+dianne44@users.noreply.github.com>
2025-02-27 16:49:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 import shap
 import xgboost as xgb
 import pandas as pd
+from datasets import load_dataset  
 
 # Load the trained model
 class QuantumFraudDetector(torch.nn.Module):
